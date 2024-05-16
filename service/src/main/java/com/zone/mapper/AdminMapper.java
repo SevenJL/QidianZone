d07daaa772814aeda5ae2fbe40403d3745cd8741
HJL <han2250027446@gmail.com>
2024-05-16 20:39:02 +0800

    个人中心和文章中心进行完善 增加用户信息管理方法 删除用户可能需要使用JWT令牌 暂不实现

--- service/src/main/java/com/zone/mapper/AdminMapper.java
+++ service/src/main/java/com/zone/mapper/AdminMapper.java
@@ -1,16 +1,22 @@
 package com.zone.mapper;
 
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface AdminMapper  {
 
+    /**
+     * 登录功能
+     */
     @Options(useGeneratedKeys = true, keyProperty = "id")
     @Select("SELECT * FROM admin WHERE name = #{name} AND password = #{password}")
     Integer Login(@Param("password") String password, @Param("name") String name);
 
+
+    /**
+     * 删除用户
+     */
+    @Delete("delete from user where id = #{id}")
+    void deleteById(@Param("id") String id);
 }
