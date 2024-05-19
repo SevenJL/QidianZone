9bcc4bba38b08473134554e2dc8afa8e078ad62d
HJL <han2250027446@gmail.com>
2024-05-19 16:08:47 +0800

    SQL语句

--- service/src/main/java/com/zone/service/CategoryService.java
+++ service/src/main/java/com/zone/service/CategoryService.java
@@ -10,14 +10,11 @@ public interface CategoryService extends IService<Category> {
 
     /**
      * 添加分类
-     * @param categoryName 分类名称
-     * @return 分类id
      */
     Integer addCategory(String categoryName);
 
     /**
      * 删除分类
-     * @param id 分类id
      */
     void deleteCategoryById(Integer id);
 
